89af69f89ec821558386f638425ba887db5e5b68
Agustin Maza <Juan.MazaEstrada@alu.frt.utn.edu.ar>
2026-06-19 22:19:24 -0300

    Consigna VI a medias

---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/PersistenceInMemory.cs
@@ -0,0 +1,38 @@
+﻿using Dsw2026Ej15.Data.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Text.Json;
+namespace Dsw2026Ej15.Data;
+
+public class PersistenceInMemory : IPersistence
+{
+    private List<Speciality> _specialities = [];
+    private List<Doctor> _doctors = [];
+
+    public PersistenceInMemory()
+    {
+        LoadSpecialities();
+    }
+
+    private void LoadSpecialities()
+    {
+        try
+        {
+            string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                "Sources", "specialities.json");
+            var json = File.ReadAllText(jsonPath);
+            var specialities = JsonSerializer.Deserialize<List<SpecialityDto>>(json,
+                new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true
+                }) ?? [];
+            _specialities = [.. specialities.Select(s => new Speciality(s.Name, s.Description, s.Id))];
+        }
+        catch (Exception)
+        {
+
+        }
+    }
+}
