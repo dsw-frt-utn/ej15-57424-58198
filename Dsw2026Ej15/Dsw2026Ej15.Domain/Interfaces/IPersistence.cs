c379ea314e7fd1541b6eddffa7eef751a447894c
Tobias Juarez <tobiasjuarez20@gmail.com>
2026-06-19 22:23:49 -0300

    Se agregan en el dominio las entidades requeridas por la consigna. Ahora compilado.

--- Dsw2026Ej15/Dsw2026Ej15.Domain/Interfaces/IPersistence.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Interfaces/IPersistence.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Dsw2026Ej15.Domain.Entities;
+namespace Dsw2026Ej15.Domain.Interfaces;
 
-namespace Dsw2026Ej15.Domain.Interfaces
+public interface IPersistence
 {
-    internal interface IPersistence
-    {
-    }
 }
